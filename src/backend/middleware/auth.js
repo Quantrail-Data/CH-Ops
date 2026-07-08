ecd0677d5de8b4daeffcb6bff553ae2316fbb025
kathirdhasan-A <kathirdhasan@quantrail-data.com>
2026-07-08 16:06:55 +0530

    update the auth middleware for validate user

--- src/backend/middleware/auth.js
+++ src/backend/middleware/auth.js
@@ -2,13 +2,23 @@
 // author -> (kathir Moorthy, kathir dhasan, Praveen kumar)
 // Middleware validating the Authorization JWT token, handling failures with 401 and attaching valid payloads to req.user.
 
+import { eq } from 'drizzle-orm';
+import { appUsers, db } from '../db/index.js';
 import { verify } from '../services/jwt.js';
 
 export function authMiddleware(req, res, next) {
   const auth = req.headers.authorization;
   if (!auth?.startsWith('Bearer ')) return res.status(401).json({ error: 'Missing authorization' });
+  const user = verify(auth.slice(7));
+  const findUser = db
+          .select()
+          .from(appUsers)
+          .where(eq(appUsers.id, user.userId))
+          .get();
+
+  if(!findUser) return res.status(401).json({error:"Oops! That user doesn't seem to exist"})
   try {
-    req.user = verify(auth.slice(7));
+    req.user = user
     next();
   } catch {
     res.status(401).json({ error: 'Invalid or expired token' });
